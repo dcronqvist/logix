0d9d12937c490ff747da7d9dea4af0ce0fe8724f
Daniel Cronqvist <dancro99@gmail.com>
2021-02-25 21:26:23 +0100

    initial commit with basic project structure and raylib dep

---
+++ LogiX/Program.cs
@@ -0,0 +1,26 @@
+﻿using Raylib_cs;
+using System;
+
+namespace LogiX
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Raylib.InitWindow(800, 480, "Hello World");
+
+            while (!Raylib.WindowShouldClose())
+            {
+                Raylib.BeginDrawing();
+                Raylib.ClearBackground(Color.WHITE);
+
+                Raylib.DrawText("Hello, world!", 12, 12, 20, Color.BLACK);
+
+                Raylib.DrawFPS(5, 5);
+                Raylib.EndDrawing();
+            }
+
+            Raylib.CloseWindow();
+        }
+    }
+}
