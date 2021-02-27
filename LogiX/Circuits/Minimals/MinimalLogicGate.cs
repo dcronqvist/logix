ad4db6403acfb74b52d2006559907b7793b52b36
Daniel Cronqvist <dancro99@gmail.com>
2021-02-27 21:25:36 +0100

    made minimal logic gate, for later when simulatin ICs

---
+++ LogiX/Circuits/Minimals/MinimalLogicGate.cs
@@ -0,0 +1,22 @@
+﻿using LogiX.Circuits.GateLogic;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LogiX.Circuits.Minimals
+{
+    class MinimalLogicGate : CircuitComponent
+    {
+        private IGateLogic logic;
+
+        public MinimalLogicGate(IGateLogic logic) : base(2, 1)
+        {
+            this.logic = logic;
+        }
+
+        protected override void PerformLogic()
+        {
+            this.Outputs[0].Value = logic.GetGateOutput(Inputs[0].Value, Inputs[1].Value);
+        }
+    }
+}
