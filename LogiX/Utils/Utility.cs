7bab480a2409bfb9d161db95f986aba78366e4e2
Daniel Cronqvist <dancro99@gmail.com>
2021-02-27 22:17:11 +0100

    made sure that path slashes are forward to be compatible with unix paths

--- LogiX/Utils/Utility.cs
+++ LogiX/Utils/Utility.cs
@@ -8,11 +8,11 @@ namespace LogiX.Utils
     static class Utility
     {
         public static string ROAMING_DIR = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
-        public static string LOGIX_DIR = ROAMING_DIR + @"\LogiX";
+        public static string LOGIX_DIR = ROAMING_DIR + @"/LogiX";
 
-        public static string SETTINGS_FILE = LOGIX_DIR + @"\settings.json";
+        public static string SETTINGS_FILE = LOGIX_DIR + @"/settings.json";
 
-        public static string LOG_FILE = LOGIX_DIR + @"\log.txt";
+        public static string LOG_FILE = LOGIX_DIR + @"/log.txt";
 
         public static Color Opacity(this Color a, float f) 
         {
