650c67d55cf56ac1ae573987699f1d3e4d189290
Daniel Cronqvist <dancro99@gmail.com>
2021-02-27 21:22:28 +0100

    made an extension method for changing the opacity of a color

--- LogiX/Utils/Utility.cs
+++ LogiX/Utils/Utility.cs
@@ -1,10 +1,11 @@
-﻿using System;
+﻿using Raylib_cs;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace LogiX.Utils
 {
-    class Utility
+    static class Utility
     {
         public static string ROAMING_DIR = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
         public static string LOGIX_DIR = ROAMING_DIR + @"\LogiX";
@@ -12,5 +13,10 @@ namespace LogiX.Utils
         public static string SETTINGS_FILE = LOGIX_DIR + @"\settings.json";
 
         public static string LOG_FILE = LOGIX_DIR + @"\log.txt";
+
+        public static Color Opacity(this Color a, float f) 
+        {
+            return new Color(a.r, a.g, a.b, (byte)(a.a * f));
+        }
     }
 }
