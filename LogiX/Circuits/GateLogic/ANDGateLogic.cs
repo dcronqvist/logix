f382f7bcb65efbad965df8109b38156aac92b053
Daniel Cronqvist <dancro99@gmail.com>
2021-02-27 21:24:22 +0100

    made an interfaceable gate logic system, so a logic gate has an implementation of its logic

---
+++ LogiX/Circuits/GateLogic/ANDGateLogic.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LogiX.Circuits.GateLogic
+{
+    class ANDGateLogic : IGateLogic
+    {
+        public LogicValue GetGateOutput(LogicValue a, LogicValue b)
+        {
+            if(a == LogicValue.NAN || b == LogicValue.NAN)
+            {
+                return LogicValue.NAN;
+            }
+            
+            if(a == LogicValue.HIGH && b == LogicValue.HIGH)
+            {
+                return LogicValue.HIGH;
+            }
+            else
+            {
+                return LogicValue.LOW;
+            }
+        }
+    }
+}
---
+++ LogiX/Circuits/GateLogic/IGateLogic.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LogiX.Circuits.GateLogic
+{
+    interface IGateLogic
+    {
+        public LogicValue GetGateOutput(LogicValue a, LogicValue b);
+    }
+}
