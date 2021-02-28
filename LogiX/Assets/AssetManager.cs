0633db2d27f50e52a75698244126f8193fdd1b36
Daniel Cronqvist <dancro99@gmail.com>
2021-02-28 01:04:06 +0100

    made some basic asset structure

---
+++ LogiX/Assets/AssetManager.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LogiX.Assets
+{
+    static class AssetManager
+    {
+        private static Dictionary<string, IAssetLoader> fileExtensionToAssetLoader;
+        private static Dictionary<string, Asset> LoadedAssets { get; set; }
+
+        private static void Initialize()
+        {
+            fileExtensionToAssetLoader = new Dictionary<string, IAssetLoader>()
+            {
+
+            };
+
+            LoadedAssets = new Dictionary<string, Asset>();
+        }
+
+        public static T GetAsset<T>(string assetName) where T : Asset
+        {
+            return LoadedAssets[assetName] as T;
+        }
+
+        public static void LoadAllAssets()
+        {
+
+        }
+    }
+}
---
+++ LogiX/Assets/IAssetLoader.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LogiX.Assets
+{
+    interface IAssetLoader
+    {
+        public Asset LoadAsset(string filePath);
+    }
+}
