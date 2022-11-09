59622bc80f79015fedcc604532a7bb567a609bf3
Daniel Cronqvist <dancro99@gmail.com>
2022-11-09 02:51:33 +0100

    Started work on adding flags register to ben eater 8 bit

--- examples/beneater-8bit/microcodegen.py
+++ examples/beneater-8bit/microcodegen.py
@@ -43,14 +43,15 @@ def print_instr(instr):
     for i in instr:
         print(f"{i[0]:08b} {i[1]:016b}")
 
-i_nop = instr(0b0000, [], [], [])                                       # NOP
-i_lda = instr(0b0001, ["MI", "IO"], ["RO", "AI"], [])                   # LDA <addr>
-i_add = instr(0b0010, ["MI", "IO"], ["RO", "BI"], ["EO", "AI"])         # ADD <addr>
-i_sub = instr(0b0011, ["MI", "IO"], ["RO", "BI"], ["EO", "SU", "AI"])   # SUB <addr>
-i_sta = instr(0b0100, ["MI", "IO"], ["AO", "RI"], [])         # STA <addr>
-i_ldi = instr(0b0101, ["IO", "AI"], [], [])                             # LDI <data>
-i_jmp = instr(0b0110, ["IO", "J"], [], [])                              # JMP <addr>
-i_out = instr(0b1110, ["AO", "OI"], [], [])                             # OUT
+i_nop = instr(0b0000, [], [], [])                                           # NOP
+i_lda = instr(0b0001, ["MI", "IO"], ["RO", "AI"], [])                       # LDA <addr>
+i_add = instr(0b0010, ["MI", "IO"], ["RO", "BI"], ["EO", "AI", "FI"])       # ADD <addr>
+i_sub = instr(0b0011, ["MI", "IO"], ["RO", "BI"], ["EO", "SU", "AI", "FI"]) # SUB <addr>
+i_sta = instr(0b0100, ["MI", "IO"], ["AO", "RI"], [])                       # STA <addr>
+i_ldi = instr(0b0101, ["IO", "AI"], [], [])                                 # LDI <data>
+i_jmp = instr(0b0110, ["IO", "J"], [], [])                                  # JMP <addr>
+i_hlt = instr(0b1111, ["HLT"], [], [])                                      # HLT 
+i_out = instr(0b1110, ["AO", "OI"], [], [])                                 # OUT
 
 all_bytes = [0x0] * 2**11 # want 11 address lines
 
@@ -62,13 +63,33 @@ instructions = [
     i_sta,
     i_ldi,
     i_jmp,
+    i_hlt,
     i_out
 ]
 
+# carry 0, zero 0
 for i in instructions:
     for j in i:
         all_bytes[j[0]] = j[1] >> 8
         all_bytes[j[0] + (1 << 7)] = j[1] & 0xFF
+
+# carry 1, zero 0
+for i in instructions:
+    for j in i:
+        all_bytes[j[0] + (1 << 8)] = j[1] >> 8
+        all_bytes[j[0] + (1 << 8) + (1 << 7)] = j[1] & 0xFF
+
+# carry 0, zero 1
+for i in instructions:
+    for j in i:
+        all_bytes[j[0] + (1 << 9)] = j[1] >> 8
+        all_bytes[j[0] + (1 << 9) + (1 << 7)] = j[1] & 0xFF
+
+# carry 1, zero 1
+for i in instructions:
+    for j in i:
+        all_bytes[j[0] + (1 << 8) + (1 << 9)] = j[1] >> 8
+        all_bytes[j[0] + (1 << 8) + (1 << 9) + (1 << 7)] = j[1] & 0xFF
 
 # output to file
 with open("microcode.bin", "wb") as f:
