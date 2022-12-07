1b170f0df8ed529a0d5510f5e880422b21ea8e2b
Daniel Cronqvist <dancro99@gmail.com>
2022-12-07 02:13:54 +0100

    Continued refactoring nodes to use new event-driven simulation

--- src/LogiX/Architecture/BuiltinComponents/Common/TextComp.cs
+++ src/LogiX/Architecture/BuiltinComponents/Common/TextComp.cs
@@ -1,111 +1,118 @@
-// using System.Drawing;
-// using System.Numerics;
-// using ImGuiNET;
-// using LogiX.Architecture.Serialization;
-// using LogiX.Content.Scripting;
-// using LogiX.Graphics;
-// using LogiX.Rendering;
+using System.Drawing;
+using System.Numerics;
+using ImGuiNET;
+using LogiX.Architecture.Serialization;
+using LogiX.Content.Scripting;
+using LogiX.Graphics;
+using LogiX.Rendering;
 
-// namespace LogiX.Architecture.BuiltinComponents;
+namespace LogiX.Architecture.BuiltinComponents;
 
-// public class TextCompData : INodeDescriptionData
-// {
-//     [NodeDescriptionProperty("Text", StringMultiline = true)]
-//     public string Text { get; set; }
+public class TextCompData : INodeDescriptionData
+{
+    [NodeDescriptionProperty("Text", StringMultiline = true)]
+    public string Text { get; set; }
 
-//     public static INodeDescriptionData GetDefault()
-//     {
-//         return new TextCompData()
-//         {
-//             Text = ""
-//         };
-//     }
-// }
+    public static INodeDescriptionData GetDefault()
+    {
+        return new TextCompData()
+        {
+            Text = ""
+        };
+    }
+}
 
-// [ScriptType("TEXTCOMP"), NodeInfo("Text", "Common", "core.markdown.text")]
-// public class TextComp : Component<TextCompData>
-// {
-//     public override string Name => "";
-//     public override bool DisplayIOGroupIdentifiers => true;
-//     public override bool ShowPropertyWindow => true;
+[ScriptType("TEXTCOMP"), NodeInfo("Text", "Common", "core.markdown.text")]
+public class TextComp : BoxNode<TextCompData>
+{
+    public override string Text => "";
+    public override float TextScale => 1f;
 
-//     private TextCompData _data;
+    private TextCompData _data;
 
-//     public override INodeDescriptionData GetDescriptionData()
-//     {
-//         return _data;
-//     }
+    public override IEnumerable<(ObservableValue, LogicValue[], int)> Evaluate(PinCollection pins)
+    {
+        yield break;
+    }
 
-//     public override void Initialize(TextCompData data)
-//     {
-//         this.ClearIOs();
-//         this._data = data;
+    public override INodeDescriptionData GetNodeData()
+    {
+        return this._data;
+    }
 
-//         this.TriggerSizeRecalculation();
-//     }
+    public override IEnumerable<PinConfig> GetPinConfiguration()
+    {
+        yield break; // No pins
+    }
 
-//     public override void PerformLogic()
-//     {
+    public override Vector2i GetSize()
+    {
+        var font = Utilities.GetFont("core.font.default", 8);
 
-//     }
+        var textScale = 1f;
+        var gridSize = Constants.GRIDSIZE;
+        var lines = this._data.Text.Split('\n');
+        var lineMeasures = lines.Select(l => font.MeasureString(l, textScale));
 
-//     public override RectangleF GetBoundingBox(out Vector2 textSize)
-//     {
-//         if (this._bounds != RectangleF.Empty)
-//         {
-//             textSize = _textSize;
-//             return this._bounds;
-//         }
+        var maxTextWidth = lineMeasures.Max(l => l.X);
+        var sumHeight = lineMeasures.Sum(l => l.Y);
 
-//         // Otherwise, calculate the size of the component.
-//         // In order to trigger a recalculation of the component's size, set _size to Vector2i.Zero.
-//         var font = Utilities.GetFont("core.font.default", 8); //LogiX.ContentManager.GetContentItem<Font>("core.font.default-regular-8");
+        var textWidth = Utilities.CeilToMultipleOf(maxTextWidth, gridSize);
+        var textHeight = Utilities.CeilToMultipleOf(sumHeight, gridSize);
 
-//         var textScale = 1f;
-//         var gridSize = Constants.GRIDSIZE;
-//         var lines = this._data.Text.Split('\n');
-//         var lineMeasures = lines.Select(l => font.MeasureString(l, textScale));
+        var size = new Vector2(Math.Max(textWidth, Constants.GRIDSIZE), Math.Max(textHeight, Constants.GRIDSIZE));
 
-//         var maxTextWidth = lineMeasures.Max(l => l.X);
-//         var sumHeight = lineMeasures.Sum(l => l.Y);
+        return new Vector2i((int)size.X.CeilToMultipleOf(Constants.GRIDSIZE) / Constants.GRIDSIZE + 2, (int)size.Y.CeilToMultipleOf(Constants.GRIDSIZE) / Constants.GRIDSIZE + 2);
+    }
 
-//         var textWidth = Utilities.CeilToMultipleOf(maxTextWidth, gridSize);
-//         var textHeight = Utilities.CeilToMultipleOf(sumHeight, gridSize);
-//         this._textSize = new Vector2(maxTextWidth, sumHeight);
+    public override void Initialize(TextCompData data)
+    {
+        this._data = data;
+    }
 
-//         var size = new Vector2(Math.Max(textWidth, Constants.GRIDSIZE), Math.Max(textHeight, Constants.GRIDSIZE));
-//         this._bounds = this.Position.ToVector2(Constants.GRIDSIZE).CreateRect(size);
-//         textSize = _textSize;
-//         return this._bounds;
-//     }
+    protected override bool Interact(Scheduler scheduler, PinCollection pins, Camera2D camera)
+    {
+        return false;
+    }
 
-//     public override void Render(Camera2D camera)
-//     {
-//         //this.TriggerSizeRecalculation();
-//         // Position of component
+    protected override IEnumerable<(ObservableValue, LogicValue[])> Prepare(PinCollection pins)
+    {
+        yield break;
+    }
 
-//         var font = Utilities.GetFont("core.font.default", 8); //LogiX.ContentManager.GetContentItem<Font>("core.font.default-regular-8");
-//         var pShader = LogiX.ContentManager.GetContentItem<ShaderProgram>("core.shader_program.primitive");
-//         var tShader = LogiX.ContentManager.GetContentItem<ShaderProgram>("core.shader_program.text");
+    public override void Render(PinCollection pins, Camera2D camera)
+    {
+        base.Render(pins, camera);
 
-//         var pos = this.Position.ToVector2(Constants.GRIDSIZE);
-//         var rect = this.GetBoundingBox(out var textSize);
-//         var size = rect.GetSize().ToVector2i(Constants.GRIDSIZE);
-//         var realSize = size.ToVector2(Constants.GRIDSIZE);
+        var font = Utilities.GetFont("core.font.default", 8);
 
-//         PrimitiveRenderer.RenderRectangle(rect, Vector2.Zero, 0f, ColorF.White);
+        var textScale = 1f;
+        var gridSize = Constants.GRIDSIZE;
+        var lines = this._data.Text.Split('\n');
+        var lineMeasures = lines.Select(l => font.MeasureString(l, textScale));
 
-//         var textLines = this._data.Text.Split('\n');
+        var maxTextWidth = lineMeasures.Max(l => l.X);
+        var sumHeight = lineMeasures.Sum(l => l.Y);
 
-//         var middle = new Vector2(rect.X + rect.Width / 2, rect.Y + rect.Height / 2);
-//         var lineSize = textSize.Y / textLines.Length;
+        var textWidth = Utilities.CeilToMultipleOf(maxTextWidth, gridSize);
+        var textHeight = Utilities.CeilToMultipleOf(sumHeight, gridSize);
 
-//         for (int i = 0; i < textLines.Length; i++)
-//         {
-//             var line = textLines[i];
-//             var linePos = new Vector2(middle.X - textSize.X / 2, middle.Y - textSize.Y / 2 + i * lineSize);
-//             TextRenderer.RenderText(font, line, linePos, 1f, 0f, ColorF.Black, camera);
-//         }
-//         //TextRenderer.RenderText(tShader, font, this.Name, textPos, 1, this.Rotation == 0 || this.Rotation == 2 ? 0f : MathF.PI / 2f, ColorF.Black, camera);
-//     }
-// }
+        var textSize = new Vector2(textWidth, textHeight);
+
+        var pos = this.Position.ToVector2(Constants.GRIDSIZE);
+        var size = this.GetSize();
+        var realSize = size.ToVector2(Constants.GRIDSIZE);
+        var rect = pos.CreateRect(realSize);
+
+        var textLines = this._data.Text.Split('\n');
+        var middle = new Vector2(rect.X + rect.Width / 2, rect.Y + rect.Height / 2);
+        var lineSize = textSize.Y / textLines.Length;
+
+        for (int i = 0; i < textLines.Length; i++)
+        {
+            var line = textLines[i];
+            var linePos = new Vector2(middle.X - textSize.X / 2, middle.Y - textSize.Y / 2 + i * lineSize);
+            TextRenderer.RenderText(font, line, linePos, 1f, 0f, ColorF.Black);
+        }
+    }
+}
--- src/LogiX/Architecture/BuiltinComponents/Common/TriStateBuffer.cs
+++ src/LogiX/Architecture/BuiltinComponents/Common/TriStateBuffer.cs
@@ -1,61 +1,115 @@
-// using ImGuiNET;
-// using LogiX.Architecture.Serialization;
-// using LogiX.Content.Scripting;
+using ImGuiNET;
+using LogiX.Architecture.Serialization;
+using LogiX.Content.Scripting;
+using LogiX.Rendering;
 
-// namespace LogiX.Architecture.BuiltinComponents;
+namespace LogiX.Architecture.BuiltinComponents;
 
-// public class NoData : INodeDescriptionData
-// {
-//     [NodeDescriptionProperty("Bits", IntMinValue = 1, IntMaxValue = 256)]
-//     public int DataBits { get; set; }
+public class NoData : INodeDescriptionData
+{
+    [NodeDescriptionProperty("Bits", IntMinValue = 1, IntMaxValue = 256)]
+    public int DataBits { get; set; }
 
-//     public static INodeDescriptionData GetDefault()
-//     {
-//         return new NoData()
-//         {
-//             DataBits = 1
-//         };
-//     }
-// }
+    public static INodeDescriptionData GetDefault()
+    {
+        return new NoData()
+        {
+            DataBits = 1
+        };
+    }
+}
 
-// [ScriptType("TRISTATE_BUFFER"), NodeInfo("TriState Buffer", "Common", "core.markdown.tristatebuffer")]
-// public class TriStateBuffer : Component<NoData>
-// {
-//     public override string Name => "TSB";
-//     public override bool DisplayIOGroupIdentifiers => true;
-//     public override bool ShowPropertyWindow => true;
+[ScriptType("TRISTATE_BUFFER"), NodeInfo("TriState Buffer", "Common", "core.markdown.tristatebuffer")]
+public class TriStateBuffer : BoxNode<NoData>
+{
+    public override string Text => "TRI";
+    public override float TextScale => 1f;
 
-//     private NoData _data;
+    private NoData _data;
 
-//     public override INodeDescriptionData GetDescriptionData()
-//     {
-//         return this._data;
-//     }
+    public override IEnumerable<(ObservableValue, LogicValue[], int)> Evaluate(PinCollection pins)
+    {
+        var pIn = pins.Get("in");
+        var pOut = pins.Get("out");
+        var pEnabled = pins.Get("enabled");
 
-//     public override void Initialize(NoData data)
-//     {
-//         this.ClearIOs();
-//         this._data = data;
+        if (pEnabled.Read().First() == LogicValue.HIGH)
+        {
+            yield return (pOut, pIn.Read(), 1);
+        }
+        else
+        {
+            yield return (pOut, LogicValue.Z.Multiple(this._data.DataBits), 1);
+        }
+    }
 
-//         this.RegisterIO("in", data.DataBits, ComponentSide.LEFT);
-//         this.RegisterIO("out", data.DataBits, ComponentSide.RIGHT);
-//         this.RegisterIO("enabled", 1, ComponentSide.TOP);
+    public override INodeDescriptionData GetNodeData()
+    {
+        return this._data;
+    }
 
-//         this.TriggerSizeRecalculation();
-//     }
+    public override IEnumerable<PinConfig> GetPinConfiguration()
+    {
+        yield return new PinConfig("in", this._data.DataBits, true, new Vector2i(0, 1));
+        yield return new PinConfig("out", this._data.DataBits, false, new Vector2i(3, 1));
+        yield return new PinConfig("enabled", 1, true, new Vector2i(1, 0));
+    }
 
-//     public override void PerformLogic()
-//     {
-//         var enabled = this.GetIOFromIdentifier("enabled").GetValues().First() == LogicValue.HIGH;
-//         var input = this.GetIOFromIdentifier("in").GetValues();
+    public override Vector2i GetSize()
+    {
+        return new Vector2i(3, 2).ApplyRotation(this.Rotation);
+    }
 
-//         if (enabled)
-//         {
-//             this.GetIOFromIdentifier("out").Push(input);
-//         }
-//         else
-//         {
-//             this.GetIOFromIdentifier("out").Push(Enumerable.Repeat(LogicValue.Z, this._data.DataBits).ToArray());
-//         }
-//     }
-// }
+    public override void Initialize(NoData data)
+    {
+        this._data = data;
+    }
+
+    protected override bool Interact(Scheduler scheduler, PinCollection pins, Camera2D camera)
+    {
+        return false;
+    }
+
+    protected override IEnumerable<(ObservableValue, LogicValue[])> Prepare(PinCollection pins)
+    {
+        yield break;
+    }
+
+    // public override string Name => "TSB";
+    // public override bool DisplayIOGroupIdentifiers => true;
+    // public override bool ShowPropertyWindow => true;
+
+    // private NoData _data;
+
+    // public override INodeDescriptionData GetDescriptionData()
+    // {
+    //     return this._data;
+    // }
+
+    // public override void Initialize(NoData data)
+    // {
+    //     this.ClearIOs();
+    //     this._data = data;
+
+    //     this.RegisterIO("in", data.DataBits, ComponentSide.LEFT);
+    //     this.RegisterIO("out", data.DataBits, ComponentSide.RIGHT);
+    //     this.RegisterIO("enabled", 1, ComponentSide.TOP);
+
+    //     this.TriggerSizeRecalculation();
+    // }
+
+    // public override void PerformLogic()
+    // {
+    //     var enabled = this.GetIOFromIdentifier("enabled").GetValues().First() == LogicValue.HIGH;
+    //     var input = this.GetIOFromIdentifier("in").GetValues();
+
+    //     if (enabled)
+    //     {
+    //         this.GetIOFromIdentifier("out").Push(input);
+    //     }
+    //     else
+    //     {
+    //         this.GetIOFromIdentifier("out").Push(Enumerable.Repeat(LogicValue.Z, this._data.DataBits).ToArray());
+    //     }
+    // }
+}
