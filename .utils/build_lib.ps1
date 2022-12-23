3cfd0ba0ad084c91455bba77ccc28afb51478912
Daniel Cronqvist <dancro99@gmail.com>
2022-12-23 16:47:16 +0100

    Removed strange argument to lib build script

--- .utils/build_lib.ps1
+++ .utils/build_lib.ps1
@@ -1,5 +1,5 @@
 Remove-Item -Path build/lib -Recurse -Force
 mkdir -Force build/lib
 
-dotnet publish src/LogiX --output "build/lib" --p:OutputType=Library -p:PublishSingleFile=false --configuration Release
+dotnet publish src/LogiX --output "build/lib" -p:OutputType=Library -p:PublishSingleFile=false --configuration Release
 
